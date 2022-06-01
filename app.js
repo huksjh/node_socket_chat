7dd8cf85eb4937e99675bec446a04ee9e80f6b4c
huks <wkdgur1209@gmail.com>
2022-06-01 13:53:58 +0900

    node socket chatting test

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const path = require("path");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+// socket.io 에  server 정보를 담아 사용
+const io = socketIO(server);
+
+app.use(express.static(path.join(__dirname, "src")));
+const PORT = process.env.PORT || 5000;
+
+// 서버로부터 넘어온 내용을 on 이란 함수로 사용
+io.on("connection", (socket) => {
+    console.log("연결이 되었습니다.");
+    // socket.on(a, b)
+    // a 첫번째 채팅아이디, b 클라이언트 넘어온 값
+    socket.on("chatting", (data) => {
+        console.log(data);
+        io.emit("chatting", data);
+    });
+});
+
+server.listen(PORT, () => console.log(`server is running ${PORT}\nhttp://localhost:${PORT}`));
---
+++ src/js/chat.js
@@ -0,0 +1,41 @@
+"use strict";
+const socket = io();
+
+// DOM
+const nickName = document.querySelector("#nickname");
+const chattingList = document.querySelector(".chatting-list");
+const chattingInput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+
+sendButton.addEventListener("click", (e) => {
+    if (chattingInput.value == "") {
+        alert("전송할 내용을 입력하세요");
+        chattingInput.focus();
+        return false;
+    }
+
+    const params = {
+        name: nickName.value,
+        message: chattingInput.value,
+    };
+
+    // 소켓으로 서버로 데이터 전송
+    // 첫번째 인자 - 채팅아이디 (채널이름)
+    // 두번째 인자 - 내용 보낼 데이터
+    socket.emit("chatting", params);
+
+    chattingInput.value = "";
+});
+
+// console.log(socket);
+
+// 서버에서 -> 클라이언드 받아오기
+socket.on("chatting", (data) => {
+    // data 서버에서 받아온 내용
+    console.log(data);
+    const { name, message } = data;
+    const li = document.createElement("li");
+
+    li.innerText = `${name}님의 내용 -> ${message}`;
+    chattingList.appendChild(li);
+});
